6a421289bbadbe21aa27dc1e48dc0a7a80fa5dfd
Fehmir <fehmir@gmail.com>
2023-10-05 16:13:10 +0200

    Testing AddCountry method

---
+++ ServiceContracts/DTO/CountryAddRequest.cs
@@ -0,0 +1,22 @@
+﻿using Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ServiceContracts.DTO
+{
+    /// <summary>
+    /// DTO for adding a new country
+    /// </summary>
+    public class CountryAddRequest
+    {
+        public string? CountryName { get; set; }
+
+        public Country ToCountry()
+        {
+            return new Country() { CountryName = CountryName };
+        }
+    }
+}
---
+++ ServiceContracts/DTO/CountryResponse.cs
@@ -0,0 +1,26 @@
+﻿using Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ServiceContracts.DTO
+{
+    /// <summary>
+    /// DTO class used as return type for most of CountriesService methods
+    /// </summary>
+    public class CountryResponse
+    {
+        public Guid CountryID { get; set; }
+        public string? CountryName { get; set; }
+    }
+
+    public static class CountryExtensions
+    {
+        public static CountryResponse ToCountryResponse(this Country country)
+        {
+            return new CountryResponse() { CountryID = country.CountryID, CountryName = country.CountryName };
+        }
+    }
+}
